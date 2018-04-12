5e17cfe9994436ee26c6a58e8ad69a147b63134f
Niko Nordström <niko.nordstrom@outlook.com>
2018-04-12 15:29:47 +0300

    Update

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Basic real time chat application made with Node.js using modules Express and Soc
 
 ## Installation (you need to have Node.js installed):
 
-1. Clone this project.
-2. Inside the cloned directory run `npm install`.
+1. Clone this repository.
+2. Inside the cloned repository run `npm install`.
 3. Then run `npm start`.
 4. You can now browse to 127.0.0.1:8080 to see the chat application in action.
--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const io = require("socket.io")(http);
 app.set("port", (process.env.PORT || 8080));
 app.use(Express.static(`${__dirname}/public`));
 
+let onlineUsers = 0;
+
 io.on("connection", socket => {
-    console.log("User has connected!");
+    onlineUsers++;
+    console.log(`There are currently ${onlineUsers} ${(onlineUsers == 1) ? "user" : "users"} online.`);
 
     socket.on("disconnect", () => {
-        console.log("User has disconnected!");
+        onlineUsers--;
+        console.log(`There are currently ${onlineUsers} ${(onlineUsers == 1) ? "user" : "users"} online.`);
     });
 
     socket.on("chat message", (msg, sender) => {
         const now = new Date(Date.now());
-        console.log(`[${now}] ${sender} said: ${msg}`);
+        console.log(`[${now.toISOString()}] ${sender} said: ${msg}`);
         io.emit("chat message", now, sender, msg);
     });
 });
