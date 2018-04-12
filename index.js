aedadb37a208c096dd422ce63ecdc147ede46ef4
Nordström Niko <s1600983@student.businesscollege.fi>
2018-04-12 09:12:30 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const Express = require("express");
+const app = new Express();
+
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.set("port", (process.env.PORT || 8080));
+app.use(Express.static(`${__dirname}/public`));
+
+io.on("connection", socket => {
+    console.log("User has connected!");
+
+    socket.on("disconnect", () => {
+        console.log("User has disconnected!");
+    });
+
+    socket.on("chat message", (msg, sender) => {
+        const now = new Date(Date.now());
+        console.log(`${now.getHours()}:${now.getMinutes()} ${sender} said: ${msg}`);
+        io.emit("chat message", now, sender, msg);
+    });
+});
+
+http.listen(app.get("port"), app.get("ip"), () => {
+    console.log(`Node app is running on port ${app.get("port")}...`);
+});
---
+++ public/js/client.js
@@ -0,0 +1,63 @@
+const socket = io();
+
+if (!localStorage.username) {
+    document.querySelector(".popup").style.display = "block";
+}
+else {
+    document.querySelector("nav ul").style.display = "block";
+    document.querySelector("#username").innerText = localStorage.username;
+}
+
+function getTimestamp(time) {
+    time = new Date(time);
+    const hours = time.getHours();
+    let minutes = time.getMinutes();
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`;
+}
+
+function appendMessageItem(time, sender, msg) {
+    const msgList = document.querySelector("#message-list");
+    const msgInfo = document.createElement("span");
+    msgInfo.innerText = `${getTimestamp(time)} ${sender}`;
+
+    const msgItem = document.createElement("li");
+    const msgText = document.createElement("p");
+    msgText.innerText = msg;
+
+    msgItem.appendChild(msgInfo);
+    msgItem.appendChild(msgText);
+    msgList.appendChild(msgItem);
+    msgList.scrollTop = msgList.scrollHeight;
+}
+
+document.querySelector("#username-form").addEventListener("submit", (event) => {
+    document.querySelector(".popup").style.display = "none";
+    localStorage.username = document.querySelector("#username-txt").value;
+    document.querySelector("nav ul").style.display = "block";
+    document.querySelector("#username").innerText = localStorage.username;
+    event.preventDefault();
+});
+
+document.querySelector("#message-form").addEventListener("submit", (event) => {
+    const message = document.querySelector("#message-txt");
+    if (localStorage.username) {
+        socket.emit("chat message", message.value, localStorage.username);
+    }
+    else {
+        document.querySelector(".popup").style.display = "block";
+    }
+    document.querySelector("#message-txt").value = "";
+    event.preventDefault();
+});
+
+document.querySelector("#username").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "block";
+});
+
+socket.on("chat message", (time, sender, msg) => {
+    appendMessageItem(time, sender, msg);
+});
